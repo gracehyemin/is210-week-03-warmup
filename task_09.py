a93b08a4256ab3548889ffa90ec13eacf200e361
Grace Park <pjeanne14@gmail.com>
2015-09-10 02:18:21 +0000

    Unless there is change in instructions online for task_09.py,  it is correct.

--- task_09.py
+++ task_09.py
@@ -2,12 +2,11 @@
 # -*- coding: utf-8 -*-
 
 """
-
 A computer that can build the question.
 
 A computer that can build anything.
 
-'''Several lines: More. And more. Final lines are coming up. Docstrings are long.'''
+Several lines: More. And more. Final lines are coming up. Docstrings are long.
 """
 THE_ANSWER_TO_EVERYTHING = 42
 
