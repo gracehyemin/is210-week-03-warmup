8f69dfdc22b6e089989e51d98d85a91bdfe67b6a
Grace Park <pjeanne14@gmail.com>
2015-09-10 03:01:02 +0000

    The == operator can be changed to != to make it an inequality. Also if FLOATVAL=0.1, then FLOATVAL is different from DECVAL which is 0.1.

--- task_12.py
+++ task_12.py
@@ -3,14 +3,9 @@
 """Task 12"""
 
 import decimal
-
+import fractions
 INTVAL = 1
 FLOATVAL = 0.1
 
-import fractions
-
 DECVAL = decimal.Decimal('0.1')
 FRACVAL = fractions.Fraction('1/10')
-
-
-#DECVAL = print dir(1/10)
